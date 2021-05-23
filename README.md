fe2122c225f229cb97882df51ebd6db001393906
Michal Kabza <mkabza@outlook.com>
2021-05-23 02:36:29 +0200

    Added Illumina adapters and 10x barcodes files

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ngsr.resources
 
 Common resources for NGS data analysis
+
+## Directories
+
+  * `adapters/` directory contains adapter sequence files for different NGS technologies
+  * `barcodes/` directory contains barcode whitelist files for different scRNA-Seq technologies
---
+++ barcodes/prepare_data.sh
@@ -0,0 +1,10 @@
+# 10x Genomics v1 chemistry
+wget -O 10xv1.txt https://github.com/10XGenomics/cellranger/raw/master/lib/python/cellranger/barcodes/737K-april-2014_rc.txt
+gzip 10xv1.txt
+
+# 10x Genomics v2 chemistry
+wget -O 10xv2.txt https://github.com/10XGenomics/cellranger/raw/master/lib/python/cellranger/barcodes/737K-august-2016.txt
+gzip 10xv2.txt
+
+# 10x Genomics v3 chemistry
+wget -O 10xv3.txt.gz https://github.com/10XGenomics/cellranger/raw/master/lib/python/cellranger/barcodes/3M-february-2018.txt.gz
